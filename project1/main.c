30511bc3f436b08078dfe30b04a3bfcbdd4d04e9
ben <ben>
2022-12-28 11:24:11 +0200

    Initial Commit

---
+++ project1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  project1
+//
+//  Created by benberni on 28/12/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
